b6248a162b2f52037bb0ebe3b5978d6d3c166080
Bala-office <preitam_rai@yahoo.com>
2020-02-24 17:34:33 +0800

    connect fixing

--- lambdas/websockets/connect.js
+++ lambdas/websockets/connect.js
@@ -23,7 +23,7 @@ exports.handler = async event => {
 
   try {
     await Dynamo.write(data, usersTableName);
-    return Responses._200({ message: "conn", data: emojiData[0] });
+    return Responses._200({ message: "connected" });
   } catch (err) {
     console.log("Error in connect.js: ", err);
     return Responses._400({ message: "Error connecting. ", err });
